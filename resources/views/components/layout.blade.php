febe6ec316ddbaf454fef27e8cbc149cb32a4e91
Teguh <teguhmad123@gmail.com>
2024-07-19 13:40:03 +0700

    ui(): enable navlink

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title; }}</title>
+    <title>{{ $title }}</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <style>
         body {
@@ -12,7 +12,7 @@
             text-align: center;
         }
 
-        @media screen and (max-width: 600px) {   
+        @media screen and (max-width: 600px) {
             body {
                 background-size: cover;
                 background-repeat: no-repeat;
@@ -21,16 +21,15 @@
         }
     </style>
     @vite('resources/css/app.css')
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
-    {{-- @livewireStyles --}}
 </head>
+
 <body class="h-full">
   <div class="min-h-full">
     <x-nav-bar/>
     <x-header :header=$header/>
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 text-white">
-        {{ $slot; }}
+        {{ $slot }}
       </div>
     </main>
   </div>
--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -9,11 +9,11 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-              <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-              <a href="/blog" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Blog</a>
-              <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-              <a href="/credit" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Credit</a>
+              <x-nav-link href="/">Home</x-nav-link>
+              <x-nav-link href="/about">About</x-nav-link>
+              <x-nav-link href="/blog">Blog</x-nav-link>
+              <x-nav-link href="/contact">Contact</x-nav-link>
+              <x-nav-link href="/credit">Credit</x-nav-link>
             </div>
           </div>
         </div>
@@ -77,11 +77,11 @@
     <div x-show="isOpen" class="md:hidden" id="mobile-menu">
       <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
-        <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-        <a href="/blog" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Blog</a>
-        <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-        <a href="/credit" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Credit</a>
+        <x-nav-link href="/">Home</x-nav-link>
+        <x-nav-link href="/about">About</x-nav-link>
+        <x-nav-link href="/blog">Blog</x-nav-link>
+        <x-nav-link href="/contact">Contact</x-nav-link>
+        <x-nav-link href="/credit">Credit</x-nav-link>
       </div>
       <div class="border-t border-gray-700 pb-3 pt-4">
         <div class="flex items-center px-5">
