b5211b4e5005cc6eb0d9b45e8958e95c8825268a
Teguh <teguhmad123@gmail.com>
2024-07-19 13:45:53 +0700

    fix(): aria navlink and better condition

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,8 +1,8 @@
+@php $active = request()->is($attributes['href']=='/' ? '/' : substr($attributes['href'], 1, strlen($attributes['href']))) @endphp
 <a 
 {{ $attributes; }} 
 class="
-{{ request()->is($attributes['href']=='/' ? '/' : substr($attributes['href'], 1, strlen($attributes['href']))) 
-? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} 
+{{  $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} 
 rounded-md px-3 py-2 text-sm font-medium" 
-aria-current="page"
+aria-current="{{ $active ? 'page' : false}}"
 >{{ $slot }}</a>
